6a4df8235053558aec0384c57ed088feae83bd28
Shannon Holmes <snholmes13@gmail.com>
2018-10-20 01:27:42 +0100

    add style, poitioning, and additional css to navbar

--- restaurant_front/src/App.js
+++ restaurant_front/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import NavBar from './NavBar.js';
-// import { Button } from 'react-bootstrap';
-import Button from 'react-bootstrap/lib/Button';
 import CustomerContainer from './containers/customers/CustomerContainer';
 import BookingContainer from './containers/bookings/BookingContainer';
 
--- restaurant_front/src/NavBar.js
+++ restaurant_front/src/NavBar.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import { Button, ButtonToolbar, Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Button, ButtonToolbar, ButtonGroup, Navbar, Nav, NavItem, Image, DropdownButton, MenuItem, Grid, Row, Col } from 'react-bootstrap';
 
 const NavBar = () => {
   return (
-    <header className="navBar">
-      <div>
-        {/* <h1 className="title">Cafe 503</h1> */}
-        <img src="/images/Cafe503Logo.jpg" alt="CafeLogo" className="logo" rounded/>
-      </div>
-      <ButtonToolbar >
 
-        <Button to="/customers">View All Customers</Button>
+      <header className="navBar">
 
-        <Button to="/bookings">View All Bookings</Button>
-        
-      </ButtonToolbar>
+        <div>
+          {/* <h1 className="title">Cafe 503</h1> */}
+          <Image src="/images/Cafe503Logo.jpg" alt="CafeLogo" className="logo" rounded/>
+        </div>
 
-    </header>
+        <Grid className="header-grid">
+
+          <Row className="row2">
+            <ButtonToolbar className="buttons">
+
+              <ButtonGroup className="home-button">
+                <Button href="/" bsStyle="warning">Home</Button>
+              </ButtonGroup>
+
+              <DropdownButton className="customer-buttons" bsStyle="warning" title="Customers" id="dropdown-customers-menu">
+                <MenuItem href="/customers" bsStyle="warning">View All Customers</MenuItem>
+                <MenuItem href="." bsStyle="warning">Edit Customer</MenuItem>
+              </DropdownButton>
+
+              <DropdownButton className="booking-buttons" bsStyle="warning" title="Bookings" id="dropdown-bookings-menu">
+                <MenuItem href="/bookings" bsStyle="warning">View All Bookings</MenuItem>
+                <MenuItem href="." bsStyle="warning">Edit Booking</MenuItem>
+              </DropdownButton>
+
+            </ButtonToolbar>
+          </Row>
+          <Row className="row1">
+            <h1>Welcome to Cafe 503!</h1>
+          </Row>
+          </Grid>
+
+        </header>
+
   )
 
 }

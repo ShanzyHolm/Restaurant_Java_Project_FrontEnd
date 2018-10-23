a126503916b73640720974cd3f5c887fe96c6cd7
Shannon Holmes <snholmes13@gmail.com>
2018-10-23 10:32:02 +0100

    add table name to booking view

--- restaurant_front/src/components/bookings/Booking.js
+++ restaurant_front/src/components/bookings/Booking.js
@@ -4,19 +4,19 @@ import moment from 'moment';
 const Booking = (props) => {
 
   const editUrl = "/bookings/" + props.booking.id + "/edit"
+  // const url = "/bookings/" + props.booking.id
 
   let date = new Date(props.booking.date);
   let formattedDate = moment(date).format("DD/MM/YY HH:mm");
 
-  // const url = "/bookings/" + props.booking.id
   return (
     <div className="booking-component">
       {/* <a href={url}> */}
-      <h4><strong>Booking Reference Number:</strong> {props.booking.id}</h4>
-      <h5><strong>Date:</strong> {formattedDate}</h5>
-      <h5><strong>Customer Name:</strong> {props.booking.customer.name}</h5>
-      <h5><strong>Table Booked:</strong> </h5>
-      <a href={editUrl}>Edit Booking</a>
+        <h4><strong>Booking Reference Number:</strong> {props.booking.id}</h4>
+        <h5><strong>Date:</strong> {formattedDate}</h5>
+        <h5><strong>Customer Name:</strong> {props.booking.customer.name}</h5>
+        <h5><strong>Table Booked:</strong> {props.booking.table.name}</h5>
+        <a href={editUrl}>Edit Booking</a>
     </div>
   )
 }
